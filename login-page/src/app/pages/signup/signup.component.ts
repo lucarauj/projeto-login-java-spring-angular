27f20e2be667e365655e43c1d4baa593121ed2a6
lucarauj <lukas.martins193@gmail.com>
2024-03-28 20:03:11 -0300

    fix: ajusta rota de signup

--- login-page/src/app/pages/signup/signup.component.ts
+++ login-page/src/app/pages/signup/signup.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { ReactiveFormsModule, Validators } from '@angular/forms';
-import { FormGroup } from '@angular/forms';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
 interface SignupForm {
-  name: FormControl;
-  email: FormControl;
-  password: FormControl;
-  confirmPassword: FormControl;
+  name: FormControl,
+  email: FormControl,
+  password: FormControl,
+  passwordConfirm: FormControl
 }
 
 @Component({
@@ -35,23 +33,24 @@ export class SignupComponent {
   constructor(
     private router: Router,
     private loginService: LoginService,
-    private toastService: ToastrService) {
-      this.signupForm = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-        confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
-      });
-  }
-
-  submit() {
-    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
-      next: () => this.toastService.success('Login feito com sucesso!'),
-      error: () => this.toastService.error('Erro inesperado! Tente novamente mais tarde.')
+    private toastService: ToastrService
+  ){
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
     })
   }
 
-  navigate() {
-    this.router.navigate(['login']);
+  submit(){
+    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
+    })
+  }
+
+  navigate(){
+    this.router.navigate(["login"])
   }
 }
